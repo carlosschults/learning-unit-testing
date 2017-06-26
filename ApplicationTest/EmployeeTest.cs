47cf6179ea447eccd097e7474eead7703aa9503e
Carlos Schults <carlos.schults@gmail.com>
2017-06-26 16:35:41 -0300

    Add necessary attributes to test class.

--- ApplicationTest/EmployeeTest.cs
+++ ApplicationTest/EmployeeTest.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace ApplicationTest
 {
+    [TestFixture]
     class EmployeeTest
     {
+        [Test]
+        public void OurFirstTestMethod()
+        {
+
+        }
     }
 }
