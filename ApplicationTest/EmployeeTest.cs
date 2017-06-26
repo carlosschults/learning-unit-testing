88ed2b2255e6468a22a3157adf2b66944a9632de
Carlos Schults <carlos.schults@gmail.com>
2017-06-26 17:30:25 -0300

    Add empty test method.

--- ApplicationTest/EmployeeTest.cs
+++ ApplicationTest/EmployeeTest.cs
@@ -9,5 +9,10 @@ namespace ApplicationTest
         {
             Assert.Pass();
         }
+
+        [Test]
+        public void IntroduceMethodShouldWorkCorrectly()
+        {
+        }
     }
 }
