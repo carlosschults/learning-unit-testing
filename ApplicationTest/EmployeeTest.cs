477fc67d74a16c3711ef146d8286666c16c77437
Carlos Schults <carlos.schults@gmail.com>
2017-07-12 13:17:22 -0300

    Add comments to explicitly show the test phases.

--- ApplicationTest/EmployeeTest.cs
+++ ApplicationTest/EmployeeTest.cs
@@ -15,9 +15,14 @@ namespace ApplicationTest
         [Test]
         public void IntroduceMethodShouldWorkCorrectly()
         {
+            // arrange
             var sut = new Employee("Alice", "Programmer", 100000);
             var expected = "Hi! My name is Alice and I work as a Programmer.";
+
+            // act
             string result = sut.Introduce();
+            
+            // assert
             Assert.AreEqual(expected, result);
         }
     }
